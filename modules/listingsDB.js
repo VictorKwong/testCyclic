c9f6328482bca9426df9896199c0b49de5e2e1c3
victorkaiwong <victorkaihong@gmail.com>
2024-01-24 20:10:12 -0500

    .env string

--- modules/listingsDB.js
+++ modules/listingsDB.js
@@ -22,9 +22,9 @@ module.exports = class ListingsDB {
   }
 
   // Pass the connection string to `initialize()`
-  initialize() {
+  initialize(abc) {
     return new Promise((resolve, reject) => {
-      const db = mongoose.createConnection("mongodb+srv://victorkaihong:6nixF7rayB21uVV2@cluster0.8evf3mn.mongodb.net/sample_airbnb");
+      const db = mongoose.createConnection(abc);
 
       db.on('error', (err) => {
         reject(err);
--- server.js
+++ server.js
@@ -9,16 +9,18 @@ const HTTP_PORT = process.env.PORT || 8080;
 
 app.use(express.json()) // for parsing application/json
 
+require('dotenv').config()
+
 // Add support for incoming JSON entities
 app.use(bodyParser.json());
 // Add support for CORS
 app.use(cors());
 
 app.get('/', (req, res) => {
-    res.send('Test the Cyclic. Pure True now!');
+    res.send(`Test the Cyclic. Pure True now! ${process.env.ABC}`);
 });
 
-db.initialize().then(()=>{
+db.initialize(process.env.MONGODB_CONN_STRING).then(()=>{
     app.listen(HTTP_PORT, ()=>{
         console.log(`server listening on: ${HTTP_PORT}`);
     });
